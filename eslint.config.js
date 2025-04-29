29e5a568250c548ca38d6d62e88b022487a7dcf2
mikybars <mperezibars@gmail.com>
2025-04-29 13:37:27 +0200

    style: add stylistic rules

---
+++ eslint.config.js
@@ -0,0 +1,19 @@
+// eslint.config.js
+import stylisticJs from '@stylistic/eslint-plugin-js';
+
+export default [
+  {
+    plugins: {
+      '@stylistic/js': stylisticJs
+    },
+    rules: {
+
+      '@stylistic/js/indent': ['error', 2],
+      '@stylistic/js/lines-between-class-members': ['warn', 'always'],
+      '@stylistic/js/no-multiple-empty-lines': ['warn', { 'max': 1 }],
+      '@stylistic/js/padded-blocks': ['warn', { 'classes': 'never' }],
+      '@stylistic/js/max-len': ['error', { 'code': 130 }],
+      '@stylistic/js/semi': ['error', 'always'],
+    }
+  }
+];
